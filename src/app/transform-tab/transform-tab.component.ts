dd7e979dac72d5a224821559164e86a8a5053f67
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-06 17:09:16 +0100

    implemented Transform model

--- src/app/transform-tab/transform-tab.component.ts
+++ src/app/transform-tab/transform-tab.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSliderChange } from '@angular/material/slider';
 import { SettingsService } from './../service/settings.service';
+import { Transform } from './transform.model';
 
 @Component({
   selector: 'app-transform-tab',
@@ -8,6 +9,20 @@ import { SettingsService } from './../service/settings.service';
   styleUrls: ['./transform-tab.component.scss']
 })
 export class TransformTabComponent implements OnInit {
+  sliderType: Transform = {
+    label: '',
+    slider: {
+      id: '',
+      minValue: 0,
+      maxValue: 0,
+      step: 0,
+      startValue: 0,
+      currentValue: 0,
+      unit: ''
+    }
+  }
+
+  items: Transform[] = [];
 
   constructor(private settingsService: SettingsService) { }
 
