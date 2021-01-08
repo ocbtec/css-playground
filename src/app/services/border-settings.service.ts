c72f764756c93414aca9c6de1d28261c38448952
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-08 12:10:33 +0100

    implemented border settings service

--- src/app/services/border-settings.service.ts
+++ src/app/services/border-settings.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Slider } from '../slider/slider.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,88 +8,49 @@ export class BorderSettingsService {
   settingsType: string = '';
   messageDynamic: string = '';
 
-  // items: Transform[] = [];
+  items: Slider[] = [];
 
-  // sizeSlider: Transform = {
-  //   label: 'Size',
-  //   slider: {
-  //     id: 'size',
-  //     minValue: 10,
-  //     maxValue: 300,
-  //     step: 1,
-  //     currentValue: 100,
-  //     unit: 'px'
-  //   }
-  // }
-  // horizontallySlider: Transform = {
-  //   label: 'Move horizontally',
-  //   slider: {
-  //     id:'hMovement',
-  //     minValue: -300,
-  //     maxValue: 300,
-  //     step: 1,
-  //     currentValue: 0,
-  //     unit: 'px'
-  //   }
-  // }
-  // verticallySlider: Transform = {
-  //   label: 'Move vertically',
-  //   slider: {
-  //     id: 'vMovement',
-  //     minValue: -300,
-  //     maxValue: 300,
-  //     step: 1,
-  //     currentValue: 0,
-  //     unit: 'px'
-  //   }
-  // }
-  // rotateSlider: Transform = {
-  //   label: 'Rotate',
-  //   slider: {
-  //     id:'rotate',
-  //     minValue: 0,
-  //     maxValue: 360,
-  //     step: 1,
-  //     currentValue: 0,
-  //     unit: '°'
-  //   }
-  // }
+  widthSlider: Slider = {
+    label: 'Width',
+    slider: {
+      id: 'width',
+      minValue: 0,
+      maxValue: 50,
+      step: 1,
+      currentValue: 2,
+      unit: 'px'
+    }
+  }
+  radiusSlider: Slider = {
+    label: 'Radius',
+    slider: {
+      id:'radius',
+      minValue: 0,
+      maxValue: 50,
+      step: 1,
+      currentValue: 0,
+      unit: '%'
+    }
+  }
 
   constructor() { }
 
-  // initializeSizeSlider() {
-  //   this.items.push(this.sizeSlider);
-  // }
+  initializeSizeSlider() {
+    this.items.push(this.widthSlider);
+  }
+  initializeMoveHorizontallySlider() {
+    this.items.push(this.radiusSlider);
+  }
 
-  // initializeMoveHorizontallySlider() {
-  //   this.items.push(this.horizontallySlider);
-  // }
+  changeSize(value: number) {
+    this.widthSlider.slider.currentValue = value;
+  }
+  moveHorizontally(value: number) {
+    this.radiusSlider.slider.currentValue = value;
+  }
 
-  // initializeMoveVerticallySlider() {
-  //   this.items.push(this.verticallySlider);
-  // }
-
-  // initializeMoveRotateSlider() {
-  //   this.items.push(this.rotateSlider);
-  // }
-
-  // changeSize(value: number) {
-  //   this.sizeSlider.slider.currentValue = value;
-  // }
-  // moveHorizontally(value: number) {
-  //   this.horizontallySlider.slider.currentValue = value;
-  // }
-  // moveVertically(value: number) {
-  //   this.verticallySlider.slider.currentValue = value;
-  // }
-  // rotate(value: number) {
-  //   this.rotateSlider.slider.currentValue = value;
-  // }
-
-  // resetTransformSettings() {
-  //   this.sizeSlider.slider.currentValue = 100;
-  //   this.horizontallySlider.slider.currentValue = 0;
-  //   this.verticallySlider.slider.currentValue = 0;
-  //   this.rotateSlider.slider.currentValue = 0;
-  // }
+  resetTransformSettings() {
+    this.widthSlider.slider.currentValue = 2;
+    this.radiusSlider.slider.currentValue = 0;
+  }
 }
