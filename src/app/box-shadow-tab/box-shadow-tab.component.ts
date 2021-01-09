9a6ead467542a263c704c4f9579565c7ed477bbb
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-09 10:26:15 +0100

    created box-shadow component

---
+++ src/app/box-shadow-tab/box-shadow-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-shadow-tab',
+  templateUrl: './box-shadow-tab.component.html',
+  styleUrls: ['./box-shadow-tab.component.scss']
+})
+export class BoxShadowTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
