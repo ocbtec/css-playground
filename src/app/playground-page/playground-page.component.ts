24a95a88e8e595db7e7f84d80a74bddc95ff2e41
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-14 17:37:04 +0100

    implemented service to set height for playground on mobile devices

--- src/app/playground-page/playground-page.component.ts
+++ src/app/playground-page/playground-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { ColorSettingsService } from '../services/color-settings.service';
+import { MobileViewService } from '../services/mobile-view.service';
 
 @Component({
   selector: 'app-playground-page',
@@ -7,35 +8,18 @@ import { ColorSettingsService } from '../services/color-settings.service';
   styleUrls: ['./playground-page.component.scss']
 })
 export class PlaygroundPageComponent {
-  screenHeight!: number;
-  screenWidth!: number;
 
-  playgroundSectionHeight! : number;
-  playgroundInputHeight! : number;
-  playgroundOutputHeight! : number;
-
-  constructor(public colorSettingsService: ColorSettingsService) { }
+  constructor(
+    public colorSettingsService: ColorSettingsService,
+    public mobileViewService: MobileViewService
+  ) { }
 
   ngOnInit() {
-    this.screenHeight = window.innerHeight;
-    this.screenWidth = window.innerWidth;
-    this.setPlaygroundHeight();
-  }
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.screenWidth = window.innerWidth;
-    this.setPlaygroundHeight();
+    this.mobileViewService.setPlaygroundHeight();
   }
 
-  setPlaygroundHeight() {
-    if (this.screenWidth > 1024) {
-      this.playgroundSectionHeight = 700;
-      this.playgroundInputHeight = 700;
-      this.playgroundOutputHeight = 700;
-    } else if (this.screenWidth <= 1024) {
-      this.playgroundSectionHeight = this.screenHeight;
-      this.playgroundInputHeight = this.screenHeight / 2;
-      this.playgroundOutputHeight = this.screenHeight / 2;
-    }
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.mobileViewService.setPlaygroundHeight();
   }
 }
