a9b8025a192953fb75d8f32e193a916cdd24670e
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-15 19:30:19 +0100

    implemented scroll to playground button

--- src/app/playground-page/playground-page.component.ts
+++ src/app/playground-page/playground-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ColorSettingsService } from '../services/color-settings.service';
 import { MobileViewService } from '../services/mobile-view.service';
 
@@ -8,6 +8,8 @@ import { MobileViewService } from '../services/mobile-view.service';
   styleUrls: ['./playground-page.component.scss']
 })
 export class PlaygroundPageComponent implements OnInit {
+  @ViewChild('playground') playground!: ElementRef;
+  showScrollButton = false;
 
   constructor(
     public colorSettingsService: ColorSettingsService,
@@ -17,9 +19,23 @@ export class PlaygroundPageComponent implements OnInit {
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.mobileViewService.setPlaygroundHeight();
+    this.checkForScroll();
   }
 
   ngOnInit() {
     this.mobileViewService.setPlaygroundHeight();
+    this.checkForScroll();
+  }
+
+  scrollToPlayground($element: any) {
+    $element.scrollIntoView({ behavior: "smooth" });
+  }
+
+  checkForScroll() {
+    if (this.mobileViewService.screenWidth <= 480) {
+      this.showScrollButton = true;
+    } else {
+      this.showScrollButton = false;
+    }
   }
 }
