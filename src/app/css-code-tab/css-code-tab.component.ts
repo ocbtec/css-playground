8066424c1a290b52afe5d83cffcceb6fb1f4f4ee
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-16 11:09:41 +0100

    added css-code-tab component

---
+++ src/app/css-code-tab/css-code-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css-code-tab',
+  templateUrl: './css-code-tab.component.html',
+  styleUrls: ['./css-code-tab.component.scss']
+})
+export class CssCodeTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
