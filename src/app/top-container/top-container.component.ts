9f0717989bc3f89bed5e8c07cd695fb96ef26a95
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-16 10:41:27 +0100

    dynamic message for using arrow keys

--- src/app/top-container/top-container.component.ts
+++ src/app/top-container/top-container.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MobileViewService } from '../services/mobile-view.service';
 
 @Component({
   selector: 'app-top-container',
   templateUrl: './top-container.component.html',
   styleUrls: ['./top-container.component.scss']
 })
-export class TopContainerComponent { }
+export class TopContainerComponent {
+  showArrowKeysMessage = true;
+
+  constructor(public mobileViewService: MobileViewService) { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenWidth();
+  }
+
+  ngOnInit() {
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth() {
+    this.showArrowKeysMessage = this.mobileViewService.screenWidth <= 480 ? false : true;
+  }
+}
