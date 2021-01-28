b5e47720a149d0445a4222cac376df46153003ca
Richard Tepfenhart <tepfenhart@gmx.de>
2021-01-28 14:36:45 +0100

    test with angular animation

--- src/app/reset-buttons/reset-buttons.component.ts
+++ src/app/reset-buttons/reset-buttons.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { TooltipPosition } from '@angular/material/tooltip';
 import { TransformSettingsService } from '../services/transform-Settings.service';
 import { BorderSettingsService } from '../services/border-settings.service';
 import { BoxShadowSettingsService } from '../services/box-shadow-settings.service';
 import { ColorSettingsService } from '../services/color-settings.service';
 import { StartPresetsService } from '../services/start-presets.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { SettingsTabComponent } from '../settings-tab/settings-tab.component';
+import { Subject } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reset-buttons',
   templateUrl: './reset-buttons.component.html',
-  styleUrls: ['./reset-buttons.component.scss']
+  styleUrls: ['./reset-buttons.component.scss'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-360deg)' })),
+      transition('default => rotated', animate('400ms linear'))
+  ])
+]
 })
-export class ResetButtonsComponent {
+export class ResetButtonsComponent implements AfterViewInit {
   @Input() messageDynamic = '';
   @Input() settingsType = '';
   position: TooltipPosition = 'above';
@@ -19,13 +31,41 @@ export class ResetButtonsComponent {
   hideDelay = 200;
   messageDefault = 'Reset ALL Settings';
 
+
+  state = 'default';
+  stateSubject = new Subject<string>();
+
   constructor(
     private transformSettingsService: TransformSettingsService,
     private borderSettingsService: BorderSettingsService,
     private boxShadowSettingsService: BoxShadowSettingsService,
     private colorsSettingsService: ColorSettingsService,
-    private startPreset: StartPresetsService
-  ) { }
+    private startPreset: StartPresetsService,
+    settingsTab: SettingsTabComponent
+  ) {
+
+    settingsTab.activeTabSubject.subscribe(() => {
+      this.rotate();
+      // this.stateSubject.next(this.state);
+    });
+    this.stateSubject.pipe(delay(0)).subscribe(currentState => {
+      this.state = currentState;
+      // console.log(currentState);
+
+    });
+
+  }
+
+  ngAfterViewInit() {
+    this.rotate();
+    // this.stateSubject.next(this.state);
+
+  }
+
+  rotate() {
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
+    this.stateSubject.next(this.state);
+  }
 
   dynamicReset(event: any) {
     const resetTab = event.target.id;
