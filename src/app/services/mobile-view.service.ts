b18c82ed674abf3f214fbccea82301977607455a
Richard Tepfenhart <tepfenhart@gmx.de>
2021-02-02 14:42:14 +0100

    refactored breakpoints in css and mobile-view.service

--- src/app/services/mobile-view.service.ts
+++ src/app/services/mobile-view.service.ts
@@ -17,21 +17,43 @@ export class MobileViewService {
   playgroundInputHeight!: number;
   playgroundOutputHeight!: number;
 
+  playgroundInputWidth!: string;
+
   setPlaygroundHeight() {
     this.checkAspectRatio();
 
-    if (this.screenWidth >= 1366 && this.screenWidth > this.screenHeight) {
+    if (this.screenWidth > 1440 && this.screenWidth > this.screenHeight) {
+      // landscape desktop screens >= 1366 x 720
       this.playgroundSectionHeight = 700;
       this.playgroundInputHeight = 700;
       this.playgroundOutputHeight = 700;
+      this.playgroundInputWidth = 'width: 640px';
+    } else if (this.screenWidth > 1024 && this.screenWidth <= 1440) {
+      // portrait tablets
+      this.playgroundSectionHeight = 700;
+      this.playgroundInputHeight = 700;
+      this.playgroundOutputHeight = 700;
+      this.playgroundInputWidth = 'width: 482px';
+      this.playgroundSectionHeight = this.playgroundInputHeight;
     } else if (this.screenWidth <= 1024 && this.screenWidth < this.screenHeight) {
+      // portrait tablets and mobiles
       this.playgroundSectionHeight = this.screenHeight;
       this.playgroundInputHeight = this.screenHeight / 2;
       this.playgroundOutputHeight = this.screenHeight / 2;
+      this.playgroundInputWidth = 'width: 100%';
     } else if (this.screenWidth <= 1024 && this.screenWidth > this.screenHeight) {
+      // landscape tablets and mobiles
       this.playgroundSectionHeight = this.screenHeight;
       this.playgroundInputHeight = this.screenHeight;
       this.playgroundOutputHeight = this.screenHeight;
+      this.playgroundInputWidth = 'width: 50%';
+    }
+
+    // prevent to stretch the playground hight too much
+    if (this.playgroundSectionHeight > 1366 && this.portrait) {
+      this.playgroundSectionHeight = 1366;
+      this.playgroundInputHeight = this.playgroundSectionHeight / 2;
+      this.playgroundOutputHeight = this.playgroundSectionHeight / 2;
     }
   }
 
@@ -50,11 +72,12 @@ export class MobileViewService {
     this.screenWidth = window.innerWidth;
 
     this.smallScreen = this.screenWidth <= 1024;
-
+    // check to assign correct css class
     if(this.smallScreen) {
       this.landscape = this.screenWidth > this.screenHeight;
       this.portrait = this.screenWidth < this.screenHeight;
-    } else {this.landscape = false;
+    } else {
+      this.landscape = false;
       this.portrait = false;
     }
   }
